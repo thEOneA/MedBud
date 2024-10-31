1c44207c6d14b00862abbefe20a684b362a6988a
Nick Shin <hvdshin@naver.com>
2024-10-31 09:48:53 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv  
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+#PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+#PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+PINECONE_API_KEY = "pcsk_3nMiAp_LGd5TtGfh5aiWcxNNuwq2vWGy5TigJLL1CvzD6MtNpskpfwSEudwC6mnJFMzzS1"
+PINECONE_API_ENV = "us-central1-gcp"
+
+embeddings = download_hugging_face_embeddings()
+
+#Initializing the Pinecone
+pinecone.init(api_key=PINECONE_API_KEY,
+              environment=PINECONE_API_ENV)
+
+index_name="medbud"
+
+#Load index
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+chain_type_kwargs={"prompt": PROMPT}
+
+llm=CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                  model_type="llama",
+                  config={'max_new_tokens':512,
+                          'temperature':0.8})
+
+
+qa=RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=docsearch.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True, 
+    chain_type_kwargs=chain_type_kwargs)
+
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+
+
+if __name__ == '__main__':
+    app.run(host = "0.0.0.0", port = 8080, debug = True)
